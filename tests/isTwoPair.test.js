4e643ebfed00f828aaafce136a02d0344561a801
Sarmand <hi.im.sarmand@hotmail.com>
2023-04-26 11:03:21 +0200

    isTwoPair.test.js is now working

--- tests/isTwoPair.test.js
+++ tests/isTwoPair.test.js
@@ -2,19 +2,14 @@ const Hand = require('../Hand');
 const CompareHands = require('../CompareHands');
 const suits = '♥♦♣♠';
 
-test('check that isTwoPair returns correct point value for a two pair hand', () => {
 
-  let hand1 = new Hand('♥9', '♦9', '♣7', '♠7', '♣5');
-  let hand2 = new Hand('♥Q', '♦Q', '♣7', '♠5', '♣6')
-  let scoreHand1 = CompareHands.isTwoPair(hand1);
-  let scoreHand2 = CompareHands.isTwoPair(hand2)
-  expect(scoreHand2).toBeGreaterThan(scoreHand1);
+
+test('Test that TwoPair returns truthy if Two pairs', () => {
+  let hand = new Hand('♥2', '♦2', '♣9', '♠3', '♠3');
+  expect(CompareHands.isTwoPair(hand)).toBeTruthy();
 });
 
-
-test('check that isTwoPair returns 0 for a non-one pair hand', () => {
-  let hand = new Hand('♥5', '♦6', '♣7', '♠6', '♣5');
-  let result = CompareHands.isTwoPair(hand);
-  let expected = 0;
-  expect(result).toBe(expected);
+test('Test that TwoPair returns falsy if not Two pairs', () => {
+  let hand = new Hand('♥A', '♦K', '♣Q', '♠J', '♠10');
+  expect(CompareHands.isTwoPair(hand)).toBeFalsy();
 });
